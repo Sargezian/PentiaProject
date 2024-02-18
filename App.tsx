10b93a1da6eebde582676da553466a7dc1dc73d0
lennartsargezian <Lennart_1997@hotmail.com>
2024-02-18 01:19:30 +0100

    Implemented Splash Screen for Android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthProvider, useAuth } from './config/AuthContext.tsx';
 import SignInScreen from './screens/SignInScreen.tsx';
 import HomeScreen from './screens/HomeScreen.tsx';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import SplashScreen from 'react-native-splash-screen';
+import {
+    Platform,
+} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
@@ -28,6 +32,11 @@ const AppStack = () => {
 };
 
 const App = () => {
+
+    useEffect(() => {
+        if (Platform.OS === 'android') SplashScreen.hide();
+    }, []);
+
   return (
       <NavigationContainer>
         <AuthProvider>
