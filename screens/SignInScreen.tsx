817c444597d1b226b3fae9b09838586ae95dac13
lennartsargezian <Lennart_1997@hotmail.com>
2024-02-18 20:33:01 +0100

    Improved LoginUI by adding BgImage

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -6,7 +6,10 @@ import FacebookSignIn from '../components/FacebookSignIn.tsx';
 const SignInScreen = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.containerHolder}>
+            <View style={styles.containerHolderOne}>
+                <Image source={require('../assets/LoginBgImage.png')} style={styles.bgImage} />
+            </View>
+            <View style={styles.containerHolderTwo}>
                 <Image source={require('../assets/SignInLogo.png')} style={styles.image} />
                 <FacebookSignIn />
                 <GoogleSignIn />
@@ -19,18 +22,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
     },
-    containerHolder: {
-        backgroundColor: 'white',
+    containerHolderOne: {
+        flex: 0.75,
+    },
+    containerHolderTwo: {
+        flex: 3,
         justifyContent: 'center',
         alignItems: 'center',
+        borderTopLeftRadius: 80,
+        backgroundColor: 'white',
     },
     image: {
         marginBottom: 30,
         width: 300,
         height: 340,
+    },
+    bgImage: {
+        width: '100%',
+        height: '150%',
     },
 });
 
